041a5cd54c19ff89136862e91da25f31ae197f01
Bearded Avenger <email@nickhaskins.com>
2014-12-20 13:18:08 -0600

    detailing toolbar

--- public/includes/class.text-editor.php
+++ public/includes/class.text-editor.php
@@ -9,24 +9,20 @@ class aesopEditorTextEditor {
 
 	function __construct() {
 
-		add_action( 'wp_footer', array($this,'editor_nav'));
-		add_action('wp_footer',array($this,'editor_confirm'));
+		add_action( 'wp_footer', array($this,'editor_controls'));
 
 	}
 
-	function editor_nav( ) {
+	function editor_controls( ) {
 
 		?><nav id="aesop-editor--controls">
 			<a href="#" id="aesop-editor--edit" class="aesop-editor--button__primary"></a>
-			<a href="#" data-post-id="<?php echo get_the_ID();?>" id="aesop-editor--save" class="aesop-editor--button aesop-editor--button__success">save</a>
-		</nav><?php
-
-	}
-
-	function editor_confirm(){
-		?>
+			<a href="#" data-post-id="<?php echo get_the_ID();?>" id="aesop-editor--save" class="aesop-editor--button aesop-editor--button__success"></a>
+		</nav>
 		<div id="aesop-editor--confirm"></div>
 		<?php
+
 	}
+
 }
 new aesopEditorTextEditor;
