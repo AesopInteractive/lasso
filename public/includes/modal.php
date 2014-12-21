b9eb69df87f89e5cbeeba1b333037ce701a061db
Bearded Avenger <email@nickhaskins.com>
2014-12-20 21:11:38 -0600

    starting  modal

--- public/includes/modal.php
+++ public/includes/modal.php
@@ -8,7 +8,12 @@
 */
 function aesop_editor_component_modal(){
 
-	$out = 'YO';
+	$out = '<div id="aesop-editor--modal">';
+		$out .= '<div class="aesop-editor--modal__inner">';
+			$out .= '<p>Component Settings</p>';
+		$out .= '</div>';
+	$out .= '</div>';
+	$out .= '<div id="aesop-editor--modal__overlay"></div>';
 
 	return $out;
 }
