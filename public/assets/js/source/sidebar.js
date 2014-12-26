330699d4a40d60a133791fadc5ba0b2bf9b9d90b
Bearded Avenger <email@nickhaskins.com>
2014-12-26 09:06:26 -0600

    organizing and fixing styles across browsers and themes

--- public/assets/js/source/sidebar.js
+++ public/assets/js/source/sidebar.js
@@ -1,61 +1,27 @@
 (function( $ ) {
 	'use strict';
 
-	// media uploader
-	var file_frame;
-	var className;
-
-	// sending file to url field
-	$(document).on('click', '#aesop-upload-img', function( e ){
-
-	    e.preventDefault();
-
-	    className = e.currentTarget.parentElement.className;
-
-	    // If the media frame already exists, reopen it.
-	    if ( file_frame ) {
-	      	file_frame.open();
-	      	return;
-	    }
-
-	    // Create the media frame.
-	    file_frame = wp.media.frames.file_frame = wp.media({
-	      	title: $( this ).data( 'uploader_title' ),
-	      	button: {
-	        	text: $( this ).data( 'uploader_button_text' ),
-	      	},
-	      	multiple: false  // Set to true to allow multiple files to be selected
-	    });
-
-	    // When an image is selected, run a callback.
-	    file_frame.on( 'select', function() {
-
-	      var attachment = file_frame.state().get('selection').first().toJSON();
-
-	      $('.aesop-generator-attr-media_upload').val(attachment.url);
-	    });
-
-	    // Finally, open the modal
-	    file_frame.open();
-	});
-
 	$(document).ready(function(){
 
+		// helper to dstry the sidebar
 		var destroySidebar = function(){
 			$('body').removeClass('aesop-sidebar-open');
 		}
 
+		// helper to set the height of the settings panel
 		var settingsHeight = function(){
+
 			$('#aesop-editor--component__settings').height( $(window).height() );
 		}
 
-
+		// call settings height on load and resie
 		settingsHeight();
 		$(window).resize(function(){
 			settingsHeight();
 			$('#aesop-editor--component__settings').perfectScrollbar('update');
 		});
 
+		// edit component button clicked let's do some settings
 		$('#aesop-component--settings__trigger').live('click',function(){
 
 			// add a body class
@@ -92,6 +58,43 @@
 			destroySidebar();
 			$('#aesop-editor--component__settings').perfectScrollbar('destroy');
 		});
+	});
+
+	// media uploader
+	var file_frame;
+	var className;
+
+	$(document).on('click', '#aesop-upload-img', function( e ){
+
+	    e.preventDefault();
+
+	    className = e.currentTarget.parentElement.className;
+
+	    // If the media frame already exists, reopen it.
+	    if ( file_frame ) {
+	      	file_frame.open();
+	      	return;
+	    }
+
+	    // Create the media frame.
+	    file_frame = wp.media.frames.file_frame = wp.media({
+	      	title: $( this ).data( 'uploader_title' ),
+	      	button: {
+	        	text: $( this ).data( 'uploader_button_text' ),
+	      	},
+	      	multiple: false  // Set to true to allow multiple files to be selected
+	    });
+
+	    // When an image is selected, run a callback.
+	    file_frame.on( 'select', function() {
+
+	      var attachment = file_frame.state().get('selection').first().toJSON();
+
+	      $('.aesop-generator-attr-media_upload').val(attachment.url);
+	    });
+
+	    // Finally, open the modal
+	    file_frame.open();
 	});
 
 })( jQuery );
