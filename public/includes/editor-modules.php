1dd593052cb88425852324f90c1d63b8fcf294ed
Bearded Avenger <email@nickhaskins.com>
2014-12-26 17:27:16 -0600

    add html/code icon

--- public/includes/editor-modules.php
+++ public/includes/editor-modules.php
@@ -81,6 +81,7 @@ function aesop_editor_text_toolbar(){
 		    <li id="aesop-toolbar--underline" ></li>
 		    <li id="aesop-toolbar--italic"></li>
 		    <li id="aesop-toolbar--strike"></li>
+		    <li id="aesop-toolbar--html"></li>
 		    <li id="aesop-toolbar--components">
 			    <ul id="aesop-toolbar--components__list">
 					<li data-type="image" title="Image" class="image"></li>
