3c66cb09cae36de7fb37ec52018ede51eff2eb5d
Bearded Avenger <email@nickhaskins.com>
2015-01-10 21:02:47 -0600

    bringing in the date picker for post scheduling

--- public/includes/class.assets.php
+++ public/includes/class.assets.php
@@ -17,6 +17,7 @@ class aesopEditorAssets {
 			wp_enqueue_script('jquery-ui-draggable');
 			wp_enqueue_script('jquery-ui-sortable');
 			wp_enqueue_script('jquery-ui-slider');
+			wp_enqueue_script('jquery-ui-datepicker');
 
 			// media uploader
 			wp_enqueue_media();
--- public/includes/editor-modules.php
+++ public/includes/editor-modules.php
@@ -303,6 +303,7 @@ function aesop_editor_component_modal(){
 			<script>
 				jQuery(document).ready(function($){
 
+					// return the right value
 					var statusReturn = function( value ) {
 
 						var out;
@@ -315,6 +316,7 @@ function aesop_editor_component_modal(){
 						return out;
 					}
 
+					// init slider
 				    $('#aesop-editor--slider').slider({
 				      	value:<?php echo $code;?>,
 				      	min: 100,
@@ -335,7 +337,15 @@ function aesop_editor_component_modal(){
 				    });
 				    $('input[name="status"]').val( statusReturn( $( "#aesop-editor--slider" ).slider('value') ) );
 
-				     $('input[name="story_slug"]').on('keyup',function(){
+				    // init datepicker
+				    $( '#story-schedule' ).datepicker({
+				    	beforeShow: function(input, inst) {
+					       $('#ui-datepicker-div').addClass('story-schedule-calendar');
+					   }
+				    });
+
+				    // if any changes happen then show the footer
+				    $('input[name="story_slug"]').on('keyup',function(){
 					  	$('.aesop-editor--postsettings__footer').slideDown()
 					});
 				});
@@ -354,10 +364,15 @@ function aesop_editor_component_modal(){
 					</div>
 				</div>
 
-				<div class="aesop-editor--postsettings__option story-slug-option aesop-editor--last-option">
+				<div class="aesop-editor--postsettings__option story-slug-option">
 					<label>URL</label>
 					<div class="url-helper"><?php echo get_bloginfo('url');?></div>
 					<input type="text" name="story_slug" value="<?php echo isset( $post ) ? esc_attr( $post->post_name ) : false;?>">
+				</div>
+
+				<div class="aesop-editor--postsettings__option story-schedule-option aesop-editor--last-option">
+					<label>Schedule Post</label>
+					<input type="text" id="story-schedule" name="story_schedule" value="">
 				</div>
 
 				<div class="aesop-editor--postsettings__footer" style="display:none;">
