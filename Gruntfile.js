07f6f68900da3630c228e515f6ee2f17fc0a8117
Bearded Avenger <email@nickhaskins.com>
2015-01-18 20:17:14 -0600

    reorganize live settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,6 +44,7 @@ module.exports = function(grunt) {
                         'public/assets/js/source/enter-editor.js',
                         'public/assets/js/source/post-settings.js',
                         'public/assets/js/source/settings-panel.js',
+                        'public/assets/js/source/settings-live-editing.js',
                         'public/assets/js/source/toolbar.js',
                         'public/assets/js/source/process-save.js',
                         'public/assets/js/source/process-gallery.js',
