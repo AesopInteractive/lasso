2129b55c6588fa75648b6e13703ae39d8c505ccc
Bearded Avenger <email@nickhaskins.com>
2015-01-20 15:43:45 -0600

    fix nonce and action for network

--- admin/includes/class.network-settings.php
+++ admin/includes/class.network-settings.php
@@ -33,12 +33,9 @@ class ahEditorNetworkSettings {
 		if ( function_exists('is_multisite') && !is_multisite() && !current_user_can('manage_network') || !is_user_logged_in() )
 			return;
 
-		if ( isset( $_POST['action'] ) && 'aesop-editor-network-settings' == $_POST['action'] && check_admin_referer( 'nonce', 'aesop_editor_network_settings' ) ) {
+		if ( isset( $_POST['action'] ) && 'aesop-editor-settings' == $_POST['action'] && check_admin_referer( 'nonce', 'aesop_editor_settings' ) ) {
 
 			$options = isset( $_POST['aesop_editor'] ) ? $_POST['aesop_editor'] : false;
-
-			//var_dump($_POST);wp_die();
-
 			$options = array_map('sanitize_text_field', $options);
 
 			update_site_option( 'aesop_editor', $options );
