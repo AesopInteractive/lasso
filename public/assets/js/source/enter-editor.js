0f4d5b566105962366f0bce98a2a7ea3ea89eef5
Bearded Avenger <email@nickhaskins.com>
2015-01-31 11:14:02 -0600

    working now

--- public/assets/js/source/enter-editor.js
+++ public/assets/js/source/enter-editor.js
@@ -229,6 +229,189 @@ jQuery(document).ready(function($){
 
 		});
 
+		///////////
+		// INITIALIZE MAPS
+		///////////
+		var mapsGoTime = function(){
+
+			var start_point = [23,234];
+			var start_zoom = 12;
+
+			var map = L.map('aesop-map-component',{
+				scrollWheelZoom: false,
+				zoom: start_zoom,
+				center: start_point
+			});
+
+			setMapCenter(start_point[0],start_point[1]);
+
+			jQuery('#lasso-map-address').geocomplete().bind('geocode:result', function(event, result){
+				var lat = result.geometry.location.k;
+				var lng = result.geometry.location.B;
+				map.panTo(new L.LatLng(lat,lng));
+				setMapCenter(lat,lng);
+			});
+
+			L.tileLayer('//{s}.tiles.mapbox.com/v3/aesopinteractive.hkoag9o3/{z}/{x}/{y}.png', {
+				maxZoom: 20
+			}).addTo(map);
+
+			// adding a new marker
+			map.on('mousedown', onMapClick);
+			map.on('dragend', onMapDrag);
+			map.on('zoomend', onMapZoom);
+
+			function setMapCenter(k, B) {
+				var ldata = encodeLocationData(k,B);
+				jQuery('input[name="ase-map-component-start-point"]').remove();
+				jQuery('.lasso-map-data').append('<input type="hidden" name="ase-map-component-start-point" data-ase="map" value="' + ldata + '">');
+				jQuery('#lasso-map-address').val(k + ', ' + B);
+			}
+
+			function setMapZoom(z) {
+				jQuery('input[name="ase-map-component-zoom"]').remove();
+				jQuery('.lasso-map-data').append('<input type="hidden" name="ase-map-component-zoom" data-ase="map" value="' + z + '">');
+			}
+
+			function onMarkerDrag(e) {
+				updateMarkerField(e.target);
+			}
+
+			function onMapDrag(e) {
+				var mapCenter = e.target.getCenter()
+				setMapCenter(rnd(mapCenter.lat),rnd(mapCenter.lng));
+			}
+
+			function onMapZoom(e) {
+				setMapZoom(e.target.getZoom());
+			}
+
+			function rnd(n) {
+				return Math.round(n * 100) / 100
+			}
+
+			function onMapClick(e) {
+
+			    var geojsonFeature = {
+
+			        "type": "Feature",
+			        "properties": {},
+			        "geometry": {
+			                "type": "Point",
+			                "coordinates": [e.latlng.lat, e.latlng.lng]
+			        }
+			    }
+
+			    var marker;
+
+			    L.geoJson(geojsonFeature, {
+
+			        pointToLayer: function(feature, latlng){
+
+			            marker = L.marker(e.latlng, {
+
+			                title: 'Resource Location',
+			                alt: 'Resource Location',
+			                riseOnHover: true,
+			                draggable: true,
+
+			            }).bindPopup("\
+			            	<input type='text' name='ase_marker_text[]' value='Location Title'>\
+			            	<a class='marker-update-button dashicons dashicons-yes'/></a>\
+			            	<a class='marker-delete-button dashicons dashicons-trash'/></a>\
+			            	");
+
+			            marker.on('popupopen', onPopupOpen);
+			            marker.on('dragend', onMarkerDrag);
+
+			            return marker;
+			        }
+			    }).addTo(map);
+
+			   	createMarkerField( marker._leaflet_id, encodeMarkerData(e.latlng.lat, e.latlng.lng, 'Location Title') );
+
+			}
+
+			// open popup
+			function onPopupOpen() {
+
+			    var tempMarker = this;
+
+			    // To remove marker on click of delete button in the popup of marker
+			    jQuery('.marker-delete-button:visible').click(function () {
+			    	jQuery('input[data-marker="' + tempMarker._leaflet_id + '"]').remove();
+			      	map.removeLayer(tempMarker);
+			    });
+
+			    // Update the title of the location
+			    jQuery('.marker-update-button:visible').click(function (t) {
+			    	var title = t.target.previousElementSibling.value;
+			    	var tdata = encodeMarkerData(tempMarker._latlng.lat, tempMarker._latlng.lng, title);
+			    	jQuery('input[data-marker="' + tempMarker._leaflet_id + '"]').val(tdata);
+			    	tempMarker.options.title = title;
+			    	tempMarker.closePopup();
+			    	tempMarker.bindPopup("\
+				            	<input type='text' name='ase_marker_text[]' value='" + title + "'>\
+				            	<a class='marker-update-button dashicons dashicons-yes'/></a>\
+				            	<a class='marker-delete-button dashicons dashicons-trash'/></a>\
+				            	");
+			    });
+			}
+
+			// create map marker
+			function createMapMarker(latlng, title) {
+	            marker = L.marker(latlng, {
+	              	title: title,
+	              	alt: title,
+	              	riseOnHover: true,
+	              	draggable: true,
+	            }).bindPopup("\
+	            	<input type='text' name='ase_marker_text[]' value='" + title + "'>\
+	            	<a class='marker-update-button dashicons dashicons-yes'/></a>\
+	            	<a class='marker-delete-button dashicons dashicons-trash'/></a>\
+	            	");
+	            marker.on('popupopen', onPopupOpen);
+	            marker.on('dragend', onMarkerDrag);
+	            return marker;
+			}
+
+			function getAllMarkers() {
+			    var allMarkersObjArray = []; // for marker objects
+			    var allMarkersGeoJsonArray = []; // for readable geoJson markers
+			    jQuery.each(map._layers, function (ml) {
+			        if (map._layers[ml].feature) {
+			          	allMarkersObjArray.push(this)
+			          	allMarkersGeoJsonArray.push(JSON.stringify(this.toGeoJSON()))
+			        }
+			    })
+			}
+
+			// let's create a hidden form element for the marker
+			function createMarkerField(mid, mdata) {
+			  	jQuery('.lasso-map-data').append('<input type="hidden" name="ase-map-component-locations[]" data-ase="map" data-marker="' + mid + '" value="' + mdata + '">');
+			}
+
+			function updateMarkerField(m) {
+				var tdata = encodeMarkerData(m._latlng.lat, m._latlng.lng, m.options.title);
+				jQuery('input[data-marker="' + m._leaflet_id + '"]').val(tdata);
+			}
+
+			// encode the information into a string
+			function encodeMarkerData(mlat, mlng, mtitle) {
+				return encodeURIComponent(JSON.stringify({lat: mlat, lng: mlng, title: mtitle}));
+			}
+
+			// encode location into a string
+			function encodeLocationData(mlat, mlng) {
+				return encodeURIComponent(JSON.stringify({lat: mlat, lng: mlng}));
+			}
+
+			// decode the information
+			function decodeMarkerData(mdata) {
+				return decodeURIComponent(JSON.parse(mdata));
+			}
+		}
+
 		/////////////////
 		/// DRAG DROP
 		///////////////////
@@ -270,17 +453,7 @@ jQuery(document).ready(function($){
 
 						// i dont think this is the right way to do this. we need to work off the map init from editor-modules--map.php
 
-						var map = L.map('aesop-map-component',{
-							scrollWheelZoom: false,
-							zoom: 12,
-							center: [29.76, -95.38]
-						});
-
-						L.tileLayer('//{s}.tiles.mapbox.com/v3/aesopinteractive.hkoag9o3/{z}/{x}/{y}.png', {
-							maxZoom: 20
-						}).addTo(map);
-
-						map.invalidateSize(false);
+						mapsGoTime()
 					}
 				}
 
