2a59855f6d39d7d094409d42a4465eb8485deeb5
Bearded Avenger <email@nickhaskins.com>
2015-02-19 16:17:48 -0600

    fixing multiple wp images in a post not firing right id to edit

--- public/assets/js/source/process-wpimg.js
+++ public/assets/js/source/process-wpimg.js
@@ -2,26 +2,21 @@
 
 	$(document).ready(function(){
 
-		$("[class*='wp-image-']").each(function() {
+		//$("[class*='wp-image-']").each(function() {
 
 			// get the attachment id from teh class wp-image-XXX, where XXX is the id of the attached iamge
 			// this oly works if the image was inserted from within the wordpress post editor
-			var id = $(this).attr('class').match(/\d+/);
 			var ase_edit_frame;
 			var className;
 
 			$(document).on('click', '#lasso--wpimg-edit',function(e){
-  				e.preventDefault();
-  				var selected_img;
-  				var clicked = $(this)
+
+  				e.preventDefault()
+  				var selected_img
+  				, 	clicked = $(this)
+  				, 	id 		= $(this).parent().next('img').attr('class').match(/\d+/);
 
 			    className = e.currentTarget.parentElement.className;
-
-			    // If the media frame already exists, reopen it.
-			    if ( ase_edit_frame ) {
-			      	ase_edit_frame.open();
-			      	return;
-			    }
 
 			    // Create the media frame.
 			    ase_edit_frame = wp.media.frames.ase_edit_frame = wp.media({
@@ -59,7 +54,7 @@
 
 			})
 
-		});
+		//});
 
 	});
 
