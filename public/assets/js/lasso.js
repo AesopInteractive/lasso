5b8726b5160252ef2a6d19964676887afb70deb6
Bearded Avenger <email@nickhaskins.com>
2015-03-29 18:29:54 -0500

    meh

--- public/assets/js/lasso.js
+++ public/assets/js/lasso.js
@@ -11410,24 +11410,31 @@ jQuery(document).ready(function($){
 (function( $ ) {
 
 
-		// method to destroy the modal
-		var destroyModal = function(){
-			$('body').removeClass('lasso-modal-open');
-			$('#lasso--all-posts__modal, #lasso--modal__overlay').remove();
-		}
+	// method to destroy the modal
+	var destroyModal = function(){
+		$('body').removeClass('lasso-modal-open');
+		$('#lasso--all-posts__modal, #lasso--modal__overlay').remove();
+	}
 
-		// modal click
-		$('#lasso--post-all').live('click',function(e){
+	// modal click
+	$('#lasso--post-all').live('click',function(e){
 
-			e.preventDefault();
+		e.preventDefault();
 
-			// add a body class
-			$('body').toggleClass('lasso-modal-open');
+		// add a body class
+		$('body').toggleClass('lasso-modal-open');
 
-			// append teh modal markup ( lasso_editor_component_modal() )
-			$('body').append(lasso_editor.allPostModal);
+		// append teh modal markup ( lasso_editor_component_modal() )
+		$('body').append(lasso_editor.allPostModal);
 
-		})
+		var postList = $('.lasso--post-list')
+
+		postList.perfectScrollbar({
+			suppressScrollX: true
+		});
+
+
+	})
 
 
 })( jQuery );
