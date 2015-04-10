9c16ceb6fb5c2e4b4ffdafde27bfc145aed346e9
Josh Pollock <jpollock412@gmail.com>
2015-04-10 18:52:58 -0400

    Move plugin boot to a separate file

---
+++ bootstrap.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Bootstrap the plugin if version check passed.
+ *
+ * @package   @lasso
+ * @author    Josh Pollock <Josh@JoshPress.net>
+ * @license   GPL-2.0+
+ */
+/*----------------------------------------------------------------------------*
+ * Register Autoloader
+ *----------------------------------------------------------------------------*/
+include_once( LASSO_DIR . '/includes/lasso_autoloader.php' );
+$loader = new lasso_autoloader();
+
+$loader->register();
+
+/*----------------------------------------------------------------------------*
+ * Public-Facing Functionality
+ *----------------------------------------------------------------------------*/
+//require_once plugin_dir_path( __FILE__ ) . 'public/class-lasso.php';
+
+$loader->addNamespace('lasso', LASSO_DIR . 'includes' );
+$loader->addNamespace('lasso_public_facing', LASSO_DIR . 'public/includes' );
+$loader->addNamespace('lasso\internal_api', LASSO_DIR . 'internal-api' );
+new lasso\internal_api\end_points();
+
+register_activation_hook( __FILE__, array( 'lasso_public_facing\lasso', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'lasso_public_facing\lasso', 'deactivate' ) );
+
+add_action( 'plugins_loaded', array( 'lasso_public_facing\lasso', 'get_instance' ) );
+
+/*----------------------------------------------------------------------------*
+ * Dashboard and Administrative Functionality
+ *----------------------------------------------------------------------------*/
+
+if ( is_admin() ) {
+	$loader->addNamespace('lasso_admin', LASSO_DIR . 'admin/includes' );
+	add_action( 'plugins_loaded', array( 'lasso_admin\load_admin', 'get_instance' ) );
+
+}
--- lasso.php
+++ lasso.php
@@ -27,39 +27,11 @@ define( 'LASSO_VERSION', '0.9.1.1' );
 define( 'LASSO_DIR', plugin_dir_path( __FILE__ ) );
 define( 'LASSO_URL', plugins_url( '', __FILE__ ) );
 
+/**
+ * Load plugin if PHP version is 5.4 or later.
+ */
 if ( version_compare( PHP_VERSION, '5.4.0', '>=' ) ) {
-	/*----------------------------------------------------------------------------*
-	 * Register Autoloader
-	 *----------------------------------------------------------------------------*/
-	include_once( LASSO_DIR . '/includes/lasso_autoloader.php' );
-	$loader = new lasso_autoloader();
-
-	$loader->register();
-
-	/*----------------------------------------------------------------------------*
-	 * Public-Facing Functionality
-	 *----------------------------------------------------------------------------*/
-	//require_once plugin_dir_path( __FILE__ ) . 'public/class-lasso.php';
-
-	$loader->addNamespace('lasso', LASSO_DIR . 'includes' );
-	$loader->addNamespace('lasso_public_facing', LASSO_DIR . 'public/includes' );
-	$loader->addNamespace('lasso\internal_api', LASSO_DIR . 'internal-api' );
-	new lasso\internal_api\end_points();
-
-	register_activation_hook( __FILE__, array( 'lasso_public_facing\lasso', 'activate' ) );
-	register_deactivation_hook( __FILE__, array( 'lasso_public_facing\lasso', 'deactivate' ) );
-
-	add_action( 'plugins_loaded', array( 'lasso_public_facing\lasso', 'get_instance' ) );
-
-	/*----------------------------------------------------------------------------*
-	 * Dashboard and Administrative Functionality
-	 *----------------------------------------------------------------------------*/
-
-	if ( is_admin() ) {
-		$loader->addNamespace('lasso_admin', LASSO_DIR . 'admin/includes' );
-		add_action( 'plugins_loaded', array( 'lasso_admin\load_admin', 'get_instance' ) );
-
-	}
+	include_once( LASSO_DIR . '/bootstrap.php' );
 
 }else{
 	//@todo error!
