c67c4b1c1b121efb969303cb1ea953582d32a2c0
Bearded Avenger <email@nickhaskins.com>
2015-05-29 16:43:43 -0500

    a more complete form with action and nonce
    ref #79

--- public/includes/option-engine.php
+++ public/includes/option-engine.php
@@ -20,12 +20,15 @@ function lasso_option_form( $name = '', $options = array() ){
 	if ( empty( $name ) || empty( $options ) || !is_array( $options ) )
 		return;
 
+	$nonce = wp_create_nonce('lasso-post-options-'.$name );
+
 	$out = sprintf('<form id="lasso--post-form-%s" class="lasso--post-form">', $name );
 
 		$out .= lasso_option_fields( $options );
-
 		$out .='<div class="form--bottom">';
-			$out .='<input type="submit" value="Save">';
+		$out .='<input type="submit" value="Save">';
+		$out .='<input type="hidden" action="lasso_process_meta">';
+		$out .= sprintf('<input type="hidden" nonce="%s">', $nonce);
 		$out .='</div>';
 
 	$out .= '</form>';
