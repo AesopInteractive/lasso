cfd4d727f2884e1dc79d3fce0a360b8afebf4b8e
hyunster <hyunsupul@yahoo.com>
2017-04-19 21:16:27 -0400

    fix tour

--- public/assets/js/source/tour.js
+++ public/assets/js/source/tour.js
@@ -44,3 +44,18 @@
 	});
 
 })( jQuery );
+
+(function( $ ) {
+	jQuery(document).ready(function($){
+
+		$('body').addClass('lasso-modal-open');
+
+		$('.lasso--loading').remove();
+		$('#lasso--tour__slides').hide().fadeIn()
+
+		$('#lasso--tour__slides').unslider({
+			dots: true,
+			delay:7000
+		});
+	});
+})( jQuery );
